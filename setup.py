20c09f9d6b82b9d138a2bbf292863cfb849ab6f2
Raffaele Colace <rafleze@powerraffo.local>
2014-02-10 13:16:39 +0100

    description

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 import highlighter
 
 setup(
@@ -8,5 +8,5 @@ setup(
     author_email='info@20tab.com',
     description='A django app permits to highlight the searched words (self.query) in a long text and truncates it, counts number of chars == max_length',
     url='https://github.com/20tab/twentytab-highlighter',
-    packages=['highlighter'],
+    packages=find_packages(),
 )
